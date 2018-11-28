9862d0f5e70889c83fca1be2d865a2bc24510595
YusofBandar <ybandar1902@gmail.com>
2018-11-28 14:31:40 +0000

    OneDrive button logins into onedrive

--- src/script.js
+++ src/script.js
@@ -1,4 +1,14 @@
 
 function OneDrive_Login(){
     console.log('onedrive login');
+
+    var appInfo = {
+        "clientId": '1a67f6f4-db2a-4298-8cf8-72946ac50669',
+        "redirectUri": "http://localhost:8080/callback.html",
+        "scopes": "sites.read.all",
+        "authServiceUri": "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
+    }
+
+    provideAppInfo(appInfo);
+    challengeForAuth();
 }
