89e94366f82f68464d576cb308804b7ea8fc3259
YusofBandar <ybandar1902@gmail.com>
2018-11-28 14:44:57 +0000

    callback html added

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ function OneDrive_Login(){
     console.log('onedrive login');
 
     var appInfo = {
-        "clientId": '1a67f6f4-db2a-4298-8cf8-72946ac50669',
+        "clientId": 'dabc0641-14b9-4c5f-8956-73693bbc3821',
         "redirectUri": "http://localhost:8080/callback.html",
         "scopes": "sites.read.all",
         "authServiceUri": "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
