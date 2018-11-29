9da35f6b97b251bf8b3d639468be41bd941e496b
YusofBandar <ybandar1902@gmail.com>
2018-11-29 13:05:35 +0000

    Commenting added to script

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 
+// Attempts to login to OneDrive
 function oneDrive_login() {
     console.log('onedrive login');
 
+    //OneDrive Application information, retrieved from Microsoft Graph API
     var appInfo = {
         "clientId": 'dabc0641-14b9-4c5f-8956-73693bbc3821',
         "redirectUri": "https://127.0.0.1:8080/callback.html",
@@ -9,25 +11,33 @@ function oneDrive_login() {
         "authServiceUri": "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
     }
 
+    //provide the app info
     provideAppInfo(appInfo);
+    //attempt login
     challengeForAuth();
 }
 
+// Downloads specific file from a OneDrive account
+// file_path : absolute path to file, example : test/test/test.txt 
 function oneDrive_download(file_path) {
     if (!is_authenticated()) {
         alert("login into onedrive")
         return;
     }
 
-
+    // Download file providing OneDrive auth token and file path
     download_folder(localStorage.getItem("oneDriveToken"), file_path).then(function(result){
+        // show contents of file
         document.getElementById("step3").style.visibility = "visible";
         document.getElementById("file_header").innerHTML = "File Path : " + file_path;
         document.getElementById("file_contents").innerHTML = "File Contents : " + result[1];
     }).catch(function(error){
+        // Un-authorized
         if(error[0] == 401){
             alert("You are unauthorized, try logging in");
-        }else if(error[0] == 404){
+        }
+        // File not found
+        else if(error[0] == 404){
             alert("item not found, check path");
         }
         else{
@@ -62,6 +72,9 @@ function download_folder(token, file_path) {
 
 }
 
+// Downloads meta-data for a specific file
+// token : OneDrive auth token
+// file_path : absolute path to file, example : test/test/test.txt 
 function donwload_metadata(token, file_path) {
     return new Promise(function (resolve, reject) {
         var URI = "https://graph.microsoft.com/v1.0/me/drive/root:/" + file_path;
@@ -84,6 +97,8 @@ function donwload_metadata(token, file_path) {
 
 }
 
+// Downloads file contents
+// download_uri : temporary authenticated uri for a file (retrieved from file meta data)  
 function download_contents(download_uri) {
     return new Promise(function (resolve, reject) {
         var download_request = new XMLHttpRequest();
@@ -102,7 +117,8 @@ function download_contents(download_uri) {
 }
 
 
-
+// Stores session token when user is authenticated into OneDrive
+// See odauthService.js
 window.onAuthenticated = function (token, authWindow) {
     if (token) {
         authWindow.close();
@@ -111,11 +127,15 @@ window.onAuthenticated = function (token, authWindow) {
     }
 }
 
+// Checks if user is authenticated
+// Returns true if authenticated, false if not
 function is_authenticated() {
     var expiresAt = JSON.parse(localStorage.getItem('oneDriveExpiresAt'));
     return new Date().getTime() < expiresAt;
 }
 
+// Store session token into local storage and token expiration time
+// token : OneDrive auth token
 function store_session(token) {
     var expiresAt = JSON.stringify(3600 * 1000 + new Date().getTime());
     localStorage.setItem("oneDriveExpiresAt", expiresAt);
