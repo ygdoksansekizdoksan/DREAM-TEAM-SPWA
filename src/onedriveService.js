c26b20b78c53592133d25e4d73800424675204e7
Yusof Bandar <ybandar1902@gmail.com>
2018-12-10 12:27:29 +0000

    moved comments for better reading

--- src/onedriveService.js
+++ src/onedriveService.js
@@ -57,28 +57,6 @@ function isValidPath(file_path){
     Downloads file contents from OneDrive using the '@microsoft.graph.downloadurl' property.
     token : OneDrive auth token
     file_path : absolute path to file, example : test/test/test.txt 
-
-    ======================================= IMPORTANT INFORMATION ABOUT THE ONEDRIVE API =============================================================
-        Downloads file contents by :
-            1) Downloading files meta-data, which includes '@microsoft.graph.downloadUrl' property
-                The '@microsoft.graph.downloadUrl' provides a temporary authenticated url to download the file contents
-            2) Using the temporary url download file contents
-                When using the '@microsoft.graph.downloadurl' property DO NOT send any any Authorisation headers. 
-                    Sending Authorisation headers will cause a :
-                    1) CORS error when requesting from client
-                    2) 404 error when requesting from server
-                For more information look at :
-                        - https://github.com/microsoftgraph/microsoft-graph-docs/issues/43
-                        - http://web.archive.org/web/20181205150708/https://github.com/microsoftgraph/microsoft-graph-docs/issues/43
-
-        OneDrive API provides a second way to download file contents, using the /content endpoint. Using the /content point client-side
-        will always cause a CORS issue. The /content endpoint returns 302 response redirecting to a temporary pre-authenticated url 
-        (the same url as '@microsoft.graph.downloadUrl')
-    
-        For more information look at :
-                                - https://docs.microsoft.com/en-gb/onedrive/developer/rest-api/api/driveitem_get_content?view=odsp-graph-online
-                                - http://web.archive.org/web/20181205150810/https://docs.microsoft.com/en-gb/onedrive/developer/rest-api/api/driveitem_get_content?view=odsp-graph-online
-    ===================================================================================================================================================
 */
 function download_folder(token, file_path) {
     return new Promise(function (resolve, reject) {
@@ -171,3 +149,28 @@ function store_session(token) {
 
 
 
+
+
+/* ======================================= IMPORTANT INFORMATION ABOUT THE ONEDRIVE API =============================================================
+Downloads file contents by :
+    1) Downloading files meta-data, which includes '@microsoft.graph.downloadUrl' property
+        The '@microsoft.graph.downloadUrl' provides a temporary authenticated url to download the file contents
+    2) Using the temporary url download file contents
+        When using the '@microsoft.graph.downloadurl' property DO NOT send any any Authorisation headers. 
+            Sending Authorisation headers will cause a :
+            1) CORS error when requesting from client
+            2) 404 error when requesting from server
+        For more information look at :
+                - https://github.com/microsoftgraph/microsoft-graph-docs/issues/43
+                - http://web.archive.org/web/20181205150708/https://github.com/microsoftgraph/microsoft-graph-docs/issues/43
+
+OneDrive API provides a second way to download file contents, using the /content endpoint. Using the /content point client-side
+will always cause a CORS issue. The /content endpoint returns 302 response redirecting to a temporary pre-authenticated url 
+(the same url as '@microsoft.graph.downloadUrl')
+
+For more information look at :
+                        - https://docs.microsoft.com/en-gb/onedrive/developer/rest-api/api/driveitem_get_content?view=odsp-graph-online
+                        - http://web.archive.org/web/20181205150810/https://docs.microsoft.com/en-gb/onedrive/developer/rest-api/api/driveitem_get_content?view=odsp-graph-online
+===================================================================================================================================================
+
+ */
